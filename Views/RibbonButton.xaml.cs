fb9a079381f6a034a31750a4dec952fcc99fcdec
amazerol <amazerol_dev@yahoo.com>
2019-06-15 16:37:16 +0200

    Initial Ribbon

---
+++ Views/RibbonButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaRibbon.Views
+{
+    public class RibbonButton : Button
+    {
+
+    }
+}
---
+++ Views/RibbonControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.VisualTree;
+using System;
+
+namespace AvaloniaRibbon.Views
+{
+    public class RibbonControl : TabControl
+    {
+
+        public static readonly StyledProperty<IBrush> RemainingTabControlHeaderColorProperty =
+            AvaloniaProperty.Register<Border, IBrush>(nameof(RemainingTabControlHeaderColor));
+
+        public IBrush RemainingTabControlHeaderColor
+        {
+            get { return GetValue(RemainingTabControlHeaderColorProperty); }
+            set { SetValue(RemainingTabControlHeaderColorProperty, value); }
+        }
+
+       
+    }
+}
