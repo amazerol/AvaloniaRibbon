36148749e339f07d7d294e2f0eb2b6f0611a677d
amazerol <amazerol_dev@yahoo.com>
2019-06-15 23:35:21 +0200

    First button with icon finally set

---
+++ Converters/BitmapValueConverter.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System;
+using Avalonia;
+using Avalonia.Data.Converters;
+using System.Globalization;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace AvaloniaRibbon.Converters
+{
+
+    public class BitmapValueConverter : IValueConverter
+    {
+        public static BitmapValueConverter Instance = new BitmapValueConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if ((string)value != String.Empty)
+                {
+                    string val = "." + (string)value;
+                    Console.WriteLine(val);
+                    return new Avalonia.Media.Imaging.Bitmap(val);
+                }
+                return null;
+            } catch
+            {
+                return null;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- Views/RibbonButton.xaml.cs
+++ Views/RibbonButton.xaml.cs
@@ -1,11 +1,46 @@
-﻿using Avalonia;
+﻿// Copyright (c) The Avalonia Project. All rights reserved.
+// Licensed under the MIT license. See licence.md file in the project root for full license information.
+
+using System;
+using System.Linq;
+using ICommand = System.Windows.Input.ICommand;
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
+using Avalonia.VisualTree;
+using System.Collections;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data.Converters;
+using System.Globalization;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 
 namespace AvaloniaRibbon.Views
 {
-    public class RibbonButton : Button
+    public class RibbonButton : ItemsControl
     {
 
+        public static readonly StyledProperty<string> TextProperty =
+            AvaloniaProperty.Register<RibbonButton, string>(nameof(Text));
+
+        public string Text
+        {
+            get { return GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+
+        public static readonly StyledProperty<string> IconPathProperty =
+            AvaloniaProperty.Register<RibbonButton, string>(nameof(IconPath));
+
+        public string IconPath
+        {
+            get { return GetValue(IconPathProperty); }
+            set { SetValue(IconPathProperty, "/Assets/RibbonIcons/settings.png"); }
+        }
     }
+
 }
